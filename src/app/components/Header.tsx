f8a8b41c9cfe7b3e1e30ddc48bf8e2ffe6ec2388
AungMyoAye <aungmyoaye101@gmail.com>
2024-07-18 11:11:28 +0630

    added darkmode components

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,16 +1,21 @@
 import Link from "next/link";
-import React from "react";
+import DarkMode from "./DarkMode";
 
 const Header = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">RARE</a>
+        <a className="btn btn-ghost text-xl bg-amber-500 dark:bg-slate-100">
+          RARE
+        </a>
       </div>
       <div className="flex-none">
+        <DarkMode />
         <ul className="menu menu-horizontal px-1">
           <li>
-            <Link href={"/"}>Home</Link>
+            <Link href={"/"} className="text-red-400 dark:text-blue-400">
+              Home
+            </Link>
           </li>
           <li>
             <Link href={"/about"}>About</Link>

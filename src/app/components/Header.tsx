5a58ceeb4840b3e7d7327042b78a55cb36240e98
AungMyoAye <aungmyoaye101@gmail.com>
2024-07-18 10:51:11 +0630

    Changed header style

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,44 +1,21 @@
+import Link from "next/link";
 import React from "react";
-import { AiFillBell } from "react-icons/ai";
-import { MdLightMode } from "react-icons/md";
-import { MdOutlineDarkMode } from "react-icons/md";
+
 const Header = () => {
   return (
     <div className="navbar bg-base-100">
-      <div className="navbar-start">
-        <a className="btn btn-ghost ">
-          <span className="text-xl bg-amber-500 px-2 rounded">RARE</span>
-          <span className="hidden md:inline text-sm ">movie</span>
-        </a>
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">RARE</a>
       </div>
-
-      <div className="navbar-end">
-        {/* darkmode switch */}
-        <label className="swap swap-rotate">
-          {/* this hidden checkbox controls the state */}
-          <input
-            type="checkbox"
-            className="theme-controller"
-            value="synthwave"
-          />
-
-          {/* sun icon */}
-          <MdLightMode />
-
-          {/* moon icon */}
-          <MdOutlineDarkMode className="text-lg" />
-        </label>
-
-        <div className="indicator">
-          <AiFillBell className="text-lg" />
-          <span className="badge badge-xs badge-primary indicator-item">1</span>
-        </div>
-
-        <div className="avatar">
-          <div className="ring-primary ring-offset-base-100 w-8 rounded-full ring ring-offset-2">
-            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-          </div>
-        </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link href={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link href={"/about"}>About</Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,21 +3,13 @@ import React from "react";
 const Hero = () => {
   return (
     <section>
-      <div
-        className="hero min-h-96"
-        style={{
-          backgroundImage:
-            "url('https://www.freepik.com/free-ai-image/scenario-inspired-by-videogame_94951532.htm#fromView=search&page=1&position=8&uuid=1abcb265-32d9-4c6d-ae81-7814aff4f675')",
-        }}
-      >
+      <div className="hero min-h-96 ">
         <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-neutral-content text-center">
+        <div className="hero-content text-neutral-content text-start justify-start">
           <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+            <h1 className="mb-5 text-5xl font-bold">Welcome</h1>
+            <p className="mb-5 text-2xl">
+              Millions of movies, TV shows and people to discover. Explore now.
             </p>
             <button className="btn btn-primary">Get Started</button>
           </div>

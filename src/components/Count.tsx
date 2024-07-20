03a60c53c35dd98046fbcfa129c9b6b9eadc9347
AungMyoAye <aungmyoaye101@gmail.com>
2024-07-20 22:11:57 +0630

    add details page

--- src/components/Count.tsx
+++ src/components/Count.tsx
@@ -6,9 +6,9 @@ const Count = ({ rating }: CountAvg) => {
   const countAvarage = Math.round(rating * 10);
   return (
     <div
-      className={` rounded-badge border-4 border-r-transparent ${
-        countAvarage > 80 ? " border-green-500" : "border-yellow-500"
-      }`}
+      className={` rounded-badge w-12 h-12 flex items-center justify-center  ${
+        countAvarage > 80 ? " bg-green-500" : "bg-yellow-500"
+      } `}
     >
       <div className="rounded-badge bg-slate-900 text-slate-200 w-10 h-10 flex items-center justify-center   ">
         <span className="text-base">{countAvarage}</span>
--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,21 +1,53 @@
 import React from "react";
-
+import Image from "next/image";
+import Count from "./Count";
 const Details = () => {
   return (
-    <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content flex-col lg:flex-row">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-          className="max-w-sm rounded-lg shadow-2xl"
+    <div
+      className="bg-auto bg-center bg-blend-multiply bg-opacity-10 w-full p-2"
+      style={{
+        backgroundImage: `url(https://image.tmdb.org/t/p/original/zCEjjb1NH3LLsWeZx47wOeqkezf.jpg )`,
+      }}
+    >
+      <div className="hero-content w-full items-start p-0  bg-slate-400 bg-opacity-80">
+        <Image
+          src={`https://image.tmdb.org/t/p/original/zCEjjb1NH3LLsWeZx47wOeqkezf.jpg  `}
+          width={300}
+          height={200}
+          alt="posters"
+          loading="lazy"
+          className="rounded"
         />
-        <div>
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
+        <div className=" px-4">
+          <h1 className="text-4xl font-bold">Monogatari</h1>
+          <div className="text-lg py-2">
+            <span>Sci-Fi & Fantasy</span>
+            <span>Action </span>
+          </div>
+          <div className="flex items-center gap-1">
+            <span>User Score</span>
+          </div>
+          <div className="flex gap-1 py-2">
+            <button className="btn btn-circle">Heart</button>
+            <button className="btn btn-circle">Heart</button>
+            <button className="btn btn-circle">Heart</button>
+          </div>
+          <div className="py-2">
+            <h2 className="text-xl font-bold">OverView</h2>
+            <p>
+              Although there are still traces of the brief period he became a
+              vampire, third-year high school student Koyomi Araragi is human
+              again. He happens upon others with their own supernatural problems
+              and finds that he can empathize. Koyomi becomes involved in their
+              lives, seeking to help them and occasionally asking for advice
+              from Meme Oshino, the homeless man who helped him become human
+              again.', poster_path: '/zCEjjb1NH3LLsWeZx47wOeqkezf.jpg
+            </p>
+          </div>
+          <div>
+            <h3 className="text-xl font-semibold">Creator</h3>
+            <p>James Gunn</p>
+          </div>
         </div>
       </div>
     </div>

dbdd99d9b7db9cdce39dcb9a22242db55355e040
AungMyoAye <aungmyoaye101@gmail.com>
2024-07-26 12:56:17 +0630

    add motion;

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 
 import "./globals.css";
 import Header from "../components/Header";
+import Transition from "@/components/Transition";
 
 export const metadata: Metadata = {
   title: "Rare Movie App",
@@ -17,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Header />
-        {children}
+        <Transition>{children}</Transition>
       </body>
     </html>
   );

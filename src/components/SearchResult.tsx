50efdc8b67631b864192b954f1fe4542d0d11db7
AungMyoAye <aungmyoaye101@gmail.com>
2024-07-29 13:07:17 +0630

    fix:errors

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -11,7 +11,7 @@ const SearchResult = ({ data, searchWord }: CardProps) => {
   return (
     <>
       <h1 className="text-center my-4 text-lg font-serif font-semibold">
-        Search results of '{searchWord}'
+        Search results of {searchWord}
       </h1>
       <div className="grid  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-4 p-4">
         {data.map((item) => (

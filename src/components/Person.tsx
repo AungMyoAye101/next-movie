51a6d0c6bb7c9bee76b49c166e1a87ba29995f22
AungMyoAye <aungmyoaye101@gmail.com>
2024-08-04 20:55:49 +0630

    add motion

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,6 +1,10 @@
+"use client";
+
+import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+
 interface PersonData {
   id: number;
   name: string;
@@ -16,9 +20,13 @@ const Person = ({ data }: DataProp) => {
   return (
     <div className=" grid grid-cols-2  sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 mx-auto  gap-4 ">
       {data.map((data) => (
-        <div
+        <motion.div
           key={data.id}
-          className="rounded bg-base-100 shadow-sm hover:shadow-lg p-2"
+          className="rounded bg-base-100 shadow-sm hover:shadow-lg p-2 hover:text-purple-500"
+          initial={{ opacity: 0, y: 60 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ ease: "easeOut", duration: 0.8 }}
+          exit={{ opacity: 0 }}
         >
           <Link href={`/person/${data.id}`}>
             <div className="overflow-hidden">
@@ -31,11 +39,18 @@ const Person = ({ data }: DataProp) => {
                 className="rounded hover:scale-110 transition-transform ease-in-out"
               />
             </div>
-            <div className="p-2">
-              <h1>{data.name || data.original_name}</h1>
-            </div>
+            <motion.div
+              className="p-2"
+              initial={{ opacity: 0, x: -60 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ ease: "easeOut", duration: 0.8 }}
+            >
+              <h1 className="text-lg font-serif font-semibold">
+                {data.name || data.original_name}
+              </h1>
+            </motion.div>
           </Link>
-        </div>
+        </motion.div>
       ))}
     </div>
   );

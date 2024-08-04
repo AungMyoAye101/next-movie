4e1f1ec29954aa41ac20ac4cbad1d85732ba523e
AungMyoAye <aungmyoaye101@gmail.com>
2024-08-04 21:06:36 +0630

    changed input

--- src/components/SearchNav.tsx
+++ src/components/SearchNav.tsx
@@ -12,73 +12,36 @@ const SearchNav = () => {
 
   return (
     <nav className=" flex justify-between items-center px-4 py-2 bg-base-200">
-      <div className="dropdown relative block md:hidden">
-        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h7"
-            />
-          </svg>
-        </div>
-        <ul
-          tabIndex={0}
-          className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow absolute left-0 "
-        >
-          <li>
-            <Link href={"/"} className="text-xl hover:text-blue-400">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link href={"/"} className="text-xl hover:text-blue-400">
-              Movie
-            </Link>
-          </li>
-          <li>
-            <Link href={`tv/${search}`} className="text-xl hover:text-blue-400">
-              TV Shows
-            </Link>
-          </li>
-        </ul>
-      </div>
-
-      <div className="items-center gap-3 hidden md:flex">
+      <div className="items-center gap-3 flex">
         <Link href={"/"} className="btn btn-circle btn-sm btn-ghost">
           <MdOutlineArrowBackIos />
         </Link>
-        <Link
-          href={"/people"}
-          className="link  text-md no-underline hover:underline px-2 "
-        >
-          People
-        </Link>
-        <Link
-          href={`/trending`}
-          className="link text-md no-underline hover:underline px-2 "
-        >
-          Trending
-        </Link>
-        <Link
-          href={`/movieTrend`}
-          className="link text-md no-underline hover:underline px-2 "
-        >
-          Movie
-        </Link>
-        <Link
-          href={`/tvTrend`}
-          className="link  text-md no-underline hover:underline px-2 "
-        >
-          TV Shows
-        </Link>
+        <div className="hidden md:flex gap-3">
+          <Link
+            href={"/people"}
+            className="link  text-md no-underline hover:underline px-2 "
+          >
+            People
+          </Link>
+          <Link
+            href={`/trending`}
+            className="link text-md no-underline hover:underline px-2 "
+          >
+            Trending
+          </Link>
+          <Link
+            href={`/movieTrend`}
+            className="link text-md no-underline hover:underline px-2 "
+          >
+            Movie
+          </Link>
+          <Link
+            href={`/tvTrend`}
+            className="link  text-md no-underline hover:underline px-2 "
+          >
+            TV Shows
+          </Link>
+        </div>
       </div>
 
       <div>
@@ -92,7 +55,7 @@ const SearchNav = () => {
             type="text"
             placeholder="Enter Search Text..."
             onChange={(e) => setSearch(e.target.value)}
-            className="input input-sm input-bordered focus:outline-none"
+            className="input  input-bordered focus:outline-none"
           />
         </form>
       </div>

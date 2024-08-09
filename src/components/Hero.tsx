941b7513773d5d4a94615271f72693153e1aaab4
AungMyoAye <aungmyoaye101@gmail.com>
2024-08-09 20:44:24 +0630

    changed hero page stye

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,13 +6,20 @@ interface BgImage {
 const Hero = ({ image }: BgImage) => {
   return (
     <>
-      <div className="h-96">
-        <img
-          src={`https://image.tmdb.org/t/p/original${image}`}
-          alt="image"
-          className="h-96 object-cover w-full  absolute left-0 right-0 -z-10 bg-opacity-50"
-        />
-        <div className="h-96 hero-overlay bg-opacity-70 absolute z-0"></div>
+      <img
+        src={`https://image.tmdb.org/t/p/original${image}`}
+        alt="image"
+        className="h-screen object-cover w-full absolute left-0 right-0 -z-20 bg-opacity-50"
+      />
+      <div className="h-screen hero-overlay bg-opacity-70 absolute -z-10"></div>
+      <div className="h-80 flex items-center justify-center">
+        <div className="max-w-screen-md mx-auto flex flex-col gap-2">
+          <h1 className="text-xl md:text-4xl font-bold font-serif">Welcome.</h1>
+          <p className="text-lg font-semibold ">
+            Millions of movies, TV shows and people to discover. Explore now.
+          </p>
+          <SearchBox />
+        </div>
       </div>
     </>
     // <div

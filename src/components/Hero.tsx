be8abf7f37551fc560d8446989073b3a94be220f
AungMyoAye <aungmyoaye101@gmail.com>
2024-08-09 20:31:21 +0630

    changed style

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,24 +5,33 @@ interface BgImage {
 }
 const Hero = ({ image }: BgImage) => {
   return (
-    <div
-      className=" min-h-96"
-      style={{
-        backgroundImage: `url(https://image.tmdb.org/t/p/original${image})`,
-        objectFit: "cover",
-      }}
-    >
-      <div className="hero-overlay bg-opacity-70 bg-slate-700"></div>
-      <div className="hero-content text-neutral-content ">
-        <div className="max-w-full">
-          <h1 className="mb-5 text-5xl font-bold">Welcome.</h1>
-          <p className="mb-5">
-            Millions of movies, TV shows and people to discover. Explore now.
-          </p>
-          <SearchBox />
-        </div>
+    <>
+      <div className="h-96">
+        <img
+          src={`https://image.tmdb.org/t/p/original${image}`}
+          alt="image"
+          className="h-96 object-cover w-full  absolute left-0 right-0 -z-10 bg-opacity-50"
+        />
+        <div className="h-96 hero-overlay bg-opacity-70 absolute z-0"></div>
       </div>
-    </div>
+    </>
+    // <div
+    //   className="hero min-h-96 bg-right  bg-no-repeat bg-contain "
+    //   style={{
+    //     backgroundImage: `url(https://image.tmdb.org/t/p/original${image})`,
+    //   }}
+    // >
+    //   <div className="hero-overlay bg-opacity-90 bg-slate-700"></div>
+    //   <div className="hero-content text-neutral-content ">
+    //     <div className="max-w-full">
+    //       <h1 className="mb-5 text-5xl font-bold">Welcome.</h1>
+    //       <p className="mb-5">
+    //         Millions of movies, TV shows and people to discover. Explore now.
+    //       </p>
+    //       <SearchBox />
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
--- src/components/NavLinkMenu.tsx
+++ src/components/NavLinkMenu.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import NavItems from "./NavItems";
 const NavLinkMenu = () => {
   return (
-    <div className="flex m-4 gap-4 ">
+    <div className="flex px-4 py-2 gap-4 ">
       <NavItems title={"Trending"} param={"trending"} />
       <NavItems title={"Top Rated"} param={"top_rated"} />
     </div>

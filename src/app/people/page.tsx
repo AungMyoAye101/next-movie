4771f436e21013d889751b8265c143b8549452e0
AungMyoAye <aungmyoaye101@gmail.com>
2024-08-11 20:20:37 +0630

    add person Data page

--- src/app/people/page.tsx
+++ src/app/people/page.tsx
@@ -1,19 +1,10 @@
-import Person from "@/components/Person";
-import SearchNav from "@/components/SearchNav";
-import React from "react";
+import PeopleData from "@/components/PeopleData";
 
 const page = async () => {
-  const res = await fetch(
-    `https://api.themoviedb.org/3/person/popular?api_key=${process.env.API_KEY}&page=3`,
-    { next: { revalidate: 3600 } }
-  );
-  const resData = await res.json();
-  const data = resData.results;
-  console.log(data);
   return (
     <div className="p-6 ">
       <h1 className="text-xl font-bold mb-4">People</h1>
-      <Person data={data} />
+      <PeopleData />
     </div>
   );
 };
---
+++ src/components/PeopleData.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Person from "./Person";
+interface NumProp {
+  num: number;
+}
+const PeopleData = async () => {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/person/popular?api_key=${process.env.API_KEY}&page=1`,
+    { next: { revalidate: 3600 } }
+  );
+  const resData = await res.json();
+  const data = resData.results;
+  return (
+    <div>
+      <Person data={data} />
+    </div>
+  );
+};
+
+export default PeopleData;

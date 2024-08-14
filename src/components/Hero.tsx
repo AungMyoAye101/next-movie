88a8c52a5bc002731044344c02ed81b22477462f
AungMyoAye <aungmyoaye101@gmail.com>
2024-08-14 20:44:07 +0630

    fix:error

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,19 +1,20 @@
 import Image from "next/image";
 import SearchBox from "./SearchBox";
+
 interface BgImage {
   image: string;
 }
 const Hero = ({ image }: BgImage) => {
+  const imageUrl = `https://image.tmdb.org/t/p/original${image}`;
   return (
-    <>
+    <div>
       <Image
-        src={`https://image.tmdb.org/t/p/original${image}`}
+        src={imageUrl || "/backdrop.jpg"}
         alt="image"
-        width={1200}
-        height={1000}
-        className="h-screen object-cover w-full absolute left-0 right-0 -z-20 bg-opacity-50"
+        fill
+        className=" object-cover w-full absolute left-0 right-0 -z-20 bg-opacity-50"
       />
-      <div className="h-screen hero-overlay bg-opacity-70 absolute -z-10"></div>
+      <div className="min-h-96 hero-overlay bg-opacity-70 absolute -z-10"></div>
       <div className="h-80 flex items-center justify-center">
         <div className="min-w-screen-sm max-w-screen-xl  mx-auto flex flex-col items-center gap-2 px-4">
           <h1 className="text-xl md:text-4xl font-bold font-serif">Welcome.</h1>
@@ -22,7 +23,7 @@ const Hero = ({ image }: BgImage) => {
           </p>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 

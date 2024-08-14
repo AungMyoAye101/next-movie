ab6211468148e4fec2609e1b5c31f90543573bd5
AungMyoAye <aungmyoaye101@gmail.com>
2024-08-14 11:03:16 +0630

    fix: review

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -6,7 +6,15 @@ const Rating = ({ rating }: Rating) => {
   return (
     <div
       className={`rounded-badge bg-slate-900 text-slate-200 size-12 flex items-center justify-center  border-4 border-l-gray-600  ${
-        rating > 79 ? " border-green-500" : "border-yellow-500"
+        rating === 0
+          ? " border-gray-500"
+          : rating < 60
+          ? "border-red-500"
+          : rating <= 79
+          ? "border-yellow-400"
+          : rating > 79
+          ? "border-green-500"
+          : ""
       } `}
     >
       <span className="text-base">{rating}</span>
--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -9,33 +9,38 @@ const Review = async ({ media, id }: CreditProps) => {
   );
   const data = await res.json();
 
-  return (
-    <section>
-      <h1 className="text-xl font-semibold font-serif text-center my-2">
-        Reviews
-      </h1>
+  const reviewList = data.results.slice(0, 4).map((item: any) => (
+    <div
+      className="min-w-full  md:min-w-80 h-40  rounded border border-gray-300 p-2 space-y-2 mx-auto"
+      key={item.id}
+    >
+      <div className="flex items-center">
+        <MdAccountCircle className="size-14 " />
+        <div>
+          <h1 className="text-lg font-semibold">{item.author}</h1>
 
-      <div className="grid md:grid-cols-2 max-w-3xl gap-4 mx-auto">
-        {data.results.slice(0, 4).map((item: any) => (
-          <div
-            className="min-w-40 md:min-w-80  rounded border border-gray-300 p-2 space-y-2 "
-            key={item.id}
-          >
-            <div className="flex items-center">
-              <MdAccountCircle className="size-14 " />
-              <div>
-                <h1 className="text-lg font-semibold">{item.author}</h1>
-
-                <DateString dateString={item.created_at} />
-              </div>
-            </div>
-            <div>
-              <p className="line-clamp-3">{item.content}</p>
-            </div>
-          </div>
-        ))}
+          <DateString dateString={item.created_at} />
+        </div>
       </div>
-    </section>
+      <div>
+        <p className="line-clamp-3">{item.content}</p>
+      </div>
+    </div>
+  ));
+  return (
+    <>
+      {data.results.legth > 1 && (
+        <section>
+          <h1 className="text-xl font-semibold font-serif text-center my-2">
+            Reviews
+          </h1>
+
+          <div className="grid md:grid-cols-2 max-w-2xl rounded  gap-4 mx-auto p-4 place-items-center">
+            {reviewList}
+          </div>
+        </section>
+      )}
+    </>
   );
 };
 

95c424cddb4a3d092c5ed6dbcdd296e8bdc57320
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-10-16 20:41:43 +0630

    add side bars

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,10 @@
 
 import Link from "next/link";
 import { useState } from "react";
-import SearchBox from "./SearchBox";
 import { ImCross } from "react-icons/im";
 import { FaSun } from "react-icons/fa6";
 import { ThemeSwitcher } from "./ThemesChanger";
+import { FaBars } from "react-icons/fa";
 
 const navLinks = [
   {
@@ -28,42 +28,50 @@ const navLinks = [
 
 const Header = () => {
   const [searchText, setSearchText] = useState("");
+  const [toggle, setToggle] = useState(false);
 
   return (
-    <header>
-      <nav className="flex justify-between items-center px-4 py-2 fixed top-0 left-0 right-0 z-50 dark:bg-neutral-800 bg-neutral-100 backdrop-blur  bg-opacity-90">
-        <a
-          href="#"
-          className=" font-serif text-orange-500 flex gap-1 items-center"
-        >
-          <h1 className="text-xl md:text-2xl lg:text-3xl font-bold">Rare</h1>
-          <span className="hidden md:block text-lg font-sans font-semibold text-orange-300">
-            -movie
-          </span>
-        </a>
-        <div className="flex items-center ">
-          {navLinks.map((item, i) => (
-            <a
-              key={i}
-              href={item.link}
-              className="text-lg lg:text-xl px-3 py-2 font-serif font-semibold"
-            >
-              {item.name}
-            </a>
-          ))}
-          <div>
-            <ThemeSwitcher />
-          </div>
-        </div>
+    <nav className="flex justify-between items-center px-4 py-2 fixed top-0 left-0 right-0 z-50 dark:bg-neutral-800 bg-neutral-100 backdrop-blur  bg-opacity-90 shadow-md">
+      <a
+        href="#"
+        className=" font-serif text-orange-500 flex gap-1 items-center"
+      >
+        <h1 className="text-xl md:text-2xl lg:text-3xl font-bold">Rare</h1>
+        <span className="hidden md:block text-lg font-sans font-semibold text-orange-300">
+          -movie
+        </span>
+      </a>
+      <div className="hidden md:flex items-center ">
+        {navLinks.map((item, i) => (
+          <a
+            key={i}
+            href={item.link}
+            className="text-lg lg:text-xl px-3 py-2 font-serif font-semibold"
+          >
+            {item.name}
+          </a>
+        ))}
         <div>
-          <input
-            type="text"
-            className="px-3 py-2 rounded-full focus:outline-none shadow-md"
-            placeholder="Search movies and tv"
-          />
+          <ThemeSwitcher />
         </div>
-      </nav>
-    </header>
+      </div>
+      <div>
+        <input
+          type="text"
+          className="px-3 py-2 rounded-full focus:outline-none shadow-md"
+          placeholder="Search movies and tv"
+        />
+      </div>
+      <div
+        className="size-8 md:hidden flex justify-center items-center shadow-lg rounded-full border border-gray-100 cursor-pointer"
+        onClick={() => setToggle((pre) => !pre)}
+      >
+        <FaBars />
+      </div>
+      {toggle && (
+        <div className="absolute right-0 top-12 w-full h-screen bg-red-900"></div>
+      )}
+    </nav>
   );
 };
 

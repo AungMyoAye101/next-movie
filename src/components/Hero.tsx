f511acabf39f68d296b4c565bd7c6cbd79903d55
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-10-16 21:04:25 +0630

    update hero bg

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,10 +6,7 @@ interface BgImage {
 }
 const Hero = () => {
   return (
-    <main
-      className=" h-[100vh] bg-center bg-no-repeat bg-cover relative flex items-center justify-center "
-      style={{ backgroundImage: "url(/cinima.jpg)" }}
-    >
+    <main className=" min-h-[60vh]  relative flex items-center justify-center  ">
       <div className="flex flex-col justify-center items-center  z-30">
         <h1 className="text-3xl md:text-4xl lg:5xl text-gray-100  font-bold font-serif z-40 text-shadow">
           Welcome.
@@ -17,6 +14,16 @@ const Hero = () => {
         <p className="text-lg md:text-xl text-gray-200 font-semibold text-center z-30 shadow">
           Millions of movies, TV shows and people to discover. Explore now.
         </p>
+      </div>
+      <div className="absolute inset-0">
+        <div className="relative w-full h-full -z-10">
+          <Image
+            src={"/cinima.jpg"}
+            fill
+            alt="cinima background"
+            className="object-cover object-center"
+          />
+        </div>
       </div>
     </main>
   );

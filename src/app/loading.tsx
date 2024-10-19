229ec37d3194a3e1dfb69d740ca85089707e53b1
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-10-19 11:09:27 +0630

    update

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className=" min-h-screen bg-black flex justify-center items-center">
+    <div className=" min-h-screen  flex justify-center items-center">
       <span className="text-white">Loading...</span>
     </div>
   );

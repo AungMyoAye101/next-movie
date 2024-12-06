92240ece41099b7bc47bbca3fef100cf151c1a31
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-12-06 19:23:33 +0630

    add prev and next btns in recommendations

--- src/components/Recommendation.tsx
+++ src/components/Recommendation.tsx
@@ -1,15 +1,41 @@
-import React from "react";
+"use client";
+import React, { useRef } from "react";
 import Card from "./Card";
 import { getRecommendations } from "@/ApiConfig";
 import { CardData, CardProps } from "./CardGridCon";
+import { FaGreaterThan, FaLessThan } from "react-icons/fa6";
 
 const Recommendation = async ({ data }: CardProps) => {
-  console.log(data);
+  const scrollContainer = useRef<HTMLDivElement>(null);
+  const scrollBtn = (isRight: boolean) => {
+    if (scrollContainer.current) {
+      scrollContainer.current.scrollBy({
+        left: isRight ? 300 : -300,
+        behavior: "smooth",
+      });
+    }
+  };
   return (
     <>
-      <section className="px-6 py-4 space-y-2">
+      <section className="px-6 py-4 space-y-2 relative">
+        <button
+          className="p-2 rounded-full absolute top-1/2 left-0 z-10 shadow-md bg-pink-500"
+          onClick={() => scrollBtn(false)}
+        >
+          <FaLessThan className="text-lg text-gray-100" />
+        </button>
+        <button
+          className="p-2 rounded-full absolute top-1/2 right-0 z-10 shadow-md bg-pink-500"
+          onClick={() => scrollBtn(true)}
+        >
+          <FaGreaterThan className="text-lg text-gray-100" />
+        </button>
         <h1 className="text-xl font-semibold font-serif ">Recommendations</h1>
-        <main className="flex overflow-hidden overflow-x-scroll gap-4 py-4 scroll-bar">
+        <main
+          ref={scrollContainer}
+          className="flex overflow-hidden overflow-x-scroll gap-4 py-4 scroll-bar"
+          style={{ scrollBehavior: "smooth" }}
+        >
           {data.map((item) => (
             <div key={item.id}>
               <Card
